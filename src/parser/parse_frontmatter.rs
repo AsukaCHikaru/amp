06abe8af5566a1fd765c6679d8e5149681316632
asukachikaru <asuka@asukawang.com>
2026-02-14 21:26:20 +0900

    feat: parse frontmatter

--- src/parser/parse_frontmatter.rs
+++ src/parser/parse_frontmatter.rs
@@ -1,9 +1,37 @@
-use std::collections::HashMap;
+use regex::Regex;
+use std::{collections::HashMap, sync::LazyLock};
+
+static FRONTMATTER_PATTERN: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"---\n+([\s\S]+)---").expect("Invalid regex"));
+static LINE_PATTERN: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"^(.+?):\s(.+)").expect("Invalid regex"));
 
 fn parse_frontmatter(input: &str) -> HashMap<String, String> {
-    let map = HashMap::new();
+    let mut map = HashMap::new();
 
-    map
+    match FRONTMATTER_PATTERN.captures(input.trim()) {
+        Some(captured) => {
+            let raw_frontmatter = captured.get(1).unwrap().as_str().to_string();
+            raw_frontmatter
+                .split("\n")
+                .filter(|line| !(line.is_empty()))
+                .map(|line| match LINE_PATTERN.captures(line) {
+                    Some(line_captured) => {
+                        let key = line_captured.get(1).unwrap().as_str().to_string();
+                        let value = line_captured.get(2).unwrap().as_str().to_string();
+                        println!("{}, {}", key, value);
+                        (key, value)
+                    }
+                    None => ("".to_string(), "".to_string()),
+                })
+                .for_each(|(key, value)| {
+                    map.insert(key, value);
+                });
+
+            map
+        }
+        None => map,
+    }
 }
 
 #[cfg(test)]
